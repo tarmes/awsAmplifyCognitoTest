2e1025b4498d2439c401a5532ea7dc5a9a2ae53b
tarmesCarved <trevor@carved.com>
2022-03-28 14:27:37 -0400

    Post Routing, Pre-Amplify

---
+++ src/Components/Hidden.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hidden() {
+    return (
+        <h1>You have successfully logged in with your token</h1>
+    )
+};
+
+export default Hidden;
---
+++ src/Components/Shown.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Shown() {
+
+    const user = "Trev"
+    
+    return (
+        <h1>This is the landing page for everyone! Hello, {user}</h1>
+    )
+};
+
+export default Shown;
